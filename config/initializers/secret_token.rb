d0a44abc261c94629ab47e74438688dc7aced18b
Vaun Podlogar <vaun@permit.com>
2013-11-15 16:52:54 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocipedia::Application.config.secret_token = '3f7d303da2771d24d6fe736d9659fd8d2f70ac640957ab0f50d6df6066285b0146dfb7790bd0556b09d274e24c9021354dd48e34615f15ea933df2467f058e9f'
